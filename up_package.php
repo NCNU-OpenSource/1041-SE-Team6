2fcbbbbe7609a7c9bf68b2dc885bd30158e951e3
Jaywier <s102213051@mail1.ncnu.edu.tw>
2015-12-24 21:51:44 +0800

    first time commit

--- up_package.php
+++ up_package.php
@@ -4,7 +4,7 @@ require("config.php");
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>carrot putto bag</title>
+<title>PutIn Package</title>
 </head>
 <body>
 <?php
@@ -13,10 +13,20 @@ if($id<=0){
 	echo "empty ID";
 	exit(0);
 }
-$sql = "update package set qty=qty+1 where id=$id;";
-mysqli_query($conn,$sql) or die("MySQL query error"); //執行SQL
-header("Location: shop.php");
-?>
+$sqluser = "select * from user;";
+$rsuser=mysqli_query($conn,$sqluser);
+$rsu=mysqli_fetch_array($rsuser);
 
+$sqlpack = "select * from package where id=$id;";
+$rspack=mysqli_query($conn,$sqlpack);
+$rsp=mysqli_fetch_array($rspack);
+if($rsu['cashinhand']>$rsp['item_price']){ //檢查夠不夠錢
+	$sql ="update user set cashinhand=cashinhand-".$rsp['item_price'].";"; 
+	mysqli_query($conn,$sql) or die("MySQL query error"); //user的錢減少
+	$sql = "update package set qty=qty+1 where id=$id;";
+	mysqli_query($conn,$sql) or die("MySQL query error"); //購買的item數量增加
+	header("Location: shop.php");
+}
+?>
 </body>
 </html>
