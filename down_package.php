731e2e79bc3cb7fd93a02af5de0b2283a22ad3e2
102213025 <s102213025@mail1.ncnu.edu.tw>
2015-12-25 04:13:52 +0800

    Create down_package.php

---
+++ down_package.php
@@ -0,0 +1,51 @@
+<?php 
+require("config.php");
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Putdown Package</title>
+</head>
+<body>
+<?php
+
+$id=(int)$_GET['id'];
+if($id<=0){
+	echo "empty ID";
+	exit(0);
+}
+
+$sqluser = "select * from user ;";
+$rsuser=mysqli_query($conn,$sqluser);
+$rsu=mysqli_fetch_array($rsuser);
+$sqlpack = "select * from package where id=$id;";
+$rspack=mysqli_query($conn,$sqlpack);
+$rsp=mysqli_fetch_array($rspack);
+if(($rsu['lovecount']>=$rsp['item_hp'])&&($rsp['qty']>=1)){ //檢查夠不夠心
+	$sql ="update user set lovecount=lovecount-".$rsp['item_hp'].";"; //user的心減少
+	 mysqli_query($conn,$sql) or die("MySQL query error");
+	$sql = "update package set qty=qty-1 where id=$id;";//數量減少
+	mysqli_query($conn,$sql) or die("MySQL query error");
+	$sql ="update user set exp=exp+".$rsp['item_exp'].";"; //經驗增加
+	
+	
+	if($rsu['exp']>=100)
+    {
+    
+	$sql ="update user set level=level+1;";
+	mysqli_query($conn,$sql) or die("MySQL query error"); 
+	$sql ="update user set exp=exp-100;";
+	
+    }
+	mysqli_query($conn,$sql) or die("MySQL query error");
+	header("Location: 02.list.php");
+	}
+	else{
+		
+	header("Location: 02.list.php");
+	}
+
+	
+?>
+</body>
+</html>
