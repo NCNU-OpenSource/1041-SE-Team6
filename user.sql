9e7cdf47f35864a96b7edb67e068714fa79978b3
102213025 <s102213025@mail1.ncnu.edu.tw>
2015-12-25 13:21:44 +0800

    Update user.sql

--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2015 年 12 月 24 日 16:26
+-- 產生時間： 2015-12-25 06:20:31
 -- 伺服器版本: 5.6.26
 -- PHP 版本： 5.6.12
 
@@ -29,15 +29,19 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `user` (
   `id` varchar(15) COLLATE utf8_bin NOT NULL,
   `password` varchar(15) COLLATE utf8_bin NOT NULL,
-  `cashinhand` int(11) NOT NULL
+  `cashinhand` int(11) NOT NULL DEFAULT '100',
+  `lovecount` int(11) NOT NULL DEFAULT '5',
+  `level` int(11) NOT NULL DEFAULT '1',
+  `exp` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- 資料表的匯出資料 `user`
 --
 
-INSERT INTO `user` (`id`, `password`, `cashinhand`) VALUES
-('userT', '9999', 100);
+INSERT INTO `user` (`id`, `password`, `cashinhand`, `lovecount`, `level`, `exp`) VALUES
+('111', '111', 0, 5, 1, 0),
+('userT', '9999', 0, 5, 1, 0);
 
 --
 -- 已匯出資料表的索引
