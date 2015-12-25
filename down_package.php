1ba12d0f81608ae187e6dca9877f3332ea4108b5
102213025 <s102213025@mail1.ncnu.edu.tw>
2015-12-25 15:19:39 +0800

    Update down_package.php

--- down_package.php
+++ down_package.php
@@ -21,9 +21,9 @@ $sqlpack = "select * from package where id=$id;";
 $rspack=mysqli_query($conn,$sqlpack);
 $rsp=mysqli_fetch_array($rspack);
 
-if($rsp['id']<=4){
+if($rsp['id']<=4&&($rsp['qty']>=1)){
 	
-	if(($rsu['lovecount']>=$rsp['item_hp'])&&($rsp['qty']>=1)){ //檢查夠不夠心
+	if($rsu['lovecount']>=$rsp['item_hp']){ //檢查夠不夠心
 		$sql ="update user set lovecount=lovecount-".$rsp['item_hp'].";"; //user的心減少
 		mysqli_query($conn,$sql) or die("MySQL query error");
 		$sql = "update package set qty=qty-1 where id=$id;";//數量減少
@@ -35,23 +35,20 @@ if($rsp['id']<=4){
 }
 
 	
-if($rsp['id']=5){
+if($rsp['id']=5&&($rsp['qty']>=1)){
 	    $sql ="update user set lovecount=lovecount+1;"; //user的心+1
 		mysqli_query($conn,$sql) or die("MySQL query error");
 		$sql = "update package set qty=qty-1 where id=$id;";//數量減少
 		mysqli_query($conn,$sql) or die("MySQL query error");
 		header("Location: 02.list.php");
 }
-if($rsp['id']=6){
+if($rsp['id']=6&&($rsp['qty']>=1)){
 	    
 		$sql = "update package set qty=qty-1 where id=$id;";//數量減少
 		mysqli_query($conn,$sql) or die("MySQL query error");
 		header("Location: 02.list.php");
 }
 
-else{
-	header("Location: 02.list.php");
-}
 
 	
 ?>
