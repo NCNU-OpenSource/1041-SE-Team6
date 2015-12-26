fea40364c84803166aff753e58a7cea7bbbe27bd
Jaywier <s102213051@mail1.ncnu.edu.tw>
2015-12-26 22:25:21 +0800

    update user.sql

--- user.sql
+++ user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.4.14
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2015-12-26: 12:42:54
--- 伺服器版本: 5.6.24
--- PHP 版本： 5.6.8
+-- 產生時間： 2015 年 12 月 26 日 15:24
+-- 伺服器版本: 5.6.26
+-- PHP 版本： 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- 資料庫： `mydb`
@@ -32,18 +32,23 @@ CREATE TABLE IF NOT EXISTS `user` (
   `cashinhand` int(11) NOT NULL DEFAULT '100',
   `lovecount` int(11) NOT NULL DEFAULT '5',
   `level` int(11) NOT NULL DEFAULT '1',
-  `exp` int(11) NOT NULL DEFAULT '0'
+  `exp` int(11) NOT NULL DEFAULT '0',
+  `carrot_qty` int(11) NOT NULL DEFAULT '0',
+  `mushroom_qty` int(11) NOT NULL DEFAULT '0',
+  `eggplant_qty` int(11) NOT NULL DEFAULT '0',
+  `corn_qty` int(11) NOT NULL DEFAULT '0',
+  `meat_qty` int(11) NOT NULL DEFAULT '0',
+  `land_qty` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- 資料表的匯出資料 `user`
 --
 
-INSERT INTO `user` (`id`, `password`, `cashinhand`, `lovecount`, `level`, `exp`) VALUES
-('111', '111', 100, 5, 1, 0),
-('test', 'test', 100, 5, 1, 0),
-('userT', '9999', 100, 5, 1, 0),
-('william', 'william', 100, 5, 1, 0);
+INSERT INTO `user` (`id`, `password`, `cashinhand`, `lovecount`, `level`, `exp`, `carrot_qty`, `mushroom_qty`, `eggplant_qty`, `corn_qty`, `meat_qty`, `land_qty`) VALUES
+('111', '111', 0, 5, 1, 0, 10, 10, 10, 10, 10, 10),
+('userT', '9999', 60, 3, 3, 0, 0, 0, 0, 0, 0, 0),
+('william', 'william', 40, 2, 4, 0, 0, 0, 0, 0, 0, 0);
 
 --
 -- 已匯出資料表的索引
