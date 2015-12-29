47bacd1bb96c6f109c69609927478edeefa43b63
102213025 <s102213025@mail1.ncnu.edu.tw>
2015-12-29 17:57:03 +0800

    Update land.sql

--- land.sql
+++ land.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2015 年 12 月 27 日 17:02
+-- 產生時間： 2015-12-29 10:56:47
 -- 伺服器版本: 5.6.26
 -- PHP 版本： 5.6.12
 
@@ -29,26 +29,27 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `land` (
   `id` int(11) NOT NULL,
   `status` int(11) NOT NULL,
-  `item_onland` int(11) NOT NULL
+  `item_onland` int(11) NOT NULL,
+  `endtime` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- 資料表的匯出資料 `land`
 --
 
-INSERT INTO `land` (`id`, `status`, `item_onland`) VALUES
-(1, 1, 0),
-(2, 1, 0),
-(3, 0, 0),
-(4, 0, 0),
-(5, 0, 0),
-(6, 0, 0),
-(7, 0, 0),
-(8, 0, 0),
-(9, 0, 0),
-(10, 0, 0),
-(11, 0, 0),
-(12, 0, 0);
+INSERT INTO `land` (`id`, `status`, `item_onland`, `endtime`) VALUES
+(1, 0, 0, 0),
+(2, 2, 1, 0),
+(3, 2, 1, 0),
+(4, 2, 1, 0),
+(5, 2, 1, 0),
+(6, 2, 3, 0),
+(7, 2, 3, 0),
+(8, 2, 1, 0),
+(9, 2, 1, 0),
+(10, 2, 1, 0),
+(11, 2, 4, 0),
+(12, 2, 3, 0);
 
 --
 -- 已匯出資料表的索引
